feb3607779d0943d7d62688b4b696e7e28d8dbf7
Pezhman Zarabadi-Poor <pzarabadip@gmail.com>
2020-10-05 23:16:40 +0100

    first version of VaspDdecWorkChain (not tested)

--- aiida_bjm/workchains/__init__.py
+++ aiida_bjm/workchains/__init__.py
@@ -3,3 +3,4 @@ from .base import VaspBaseWorkChain
 from .vasp_multistage import VaspMultiStageWorkChain
 from .vasp_converge import VaspConvergeWorkChain
 from .vasp_catmat import VaspCatMatWorkChain
+from .vasp_ddec import VaspDdecWorkChain
